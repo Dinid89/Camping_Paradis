1be7560d6dc5e46f7d9393d0979727a97daf0dd5
David A <david.amelin62@gmail.com>
2026-06-02 20:55:27 +0200

    Middleware JWT Token

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,12 @@ require ('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const authRoutes = require('./src/routes/authRoutes')
+const { authMiddleware } = require('./src/middlewares/authMiddleware')
 
 const db = require('./src/config/db')
 
 //app
 const app = express()
-
 
 //middleware
 app.use(cors())
@@ -19,6 +19,11 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.json({ message: 'Serveur Camping Paradis opérationnel'})
 })
+
+app.get('/api/protected', authMiddleware, (req, res) => {
+  res.json({message : "Accès autorisé", user: req.user })
+})
+
 app.use('/api/auth', authRoutes)
 
 
