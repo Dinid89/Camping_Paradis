aa86a7b57af4955bf138e916750961b44a5f502a
David A <david.amelin62@gmail.com>
2026-06-02 21:45:59 +0200

    routes GET hébergements par ID #1

--- Backend/src/controllers/hebergementController.js
+++ Backend/src/controllers/hebergementController.js
@@ -1,5 +1,6 @@
 const db = require('../config/db')
 
+//Recupere tous les hergements
 const getAllHebergement = (req, res) => {
     const sql = 'SELECT * FROM hebergements'
     db.query(sql, (err, result) => {
@@ -10,4 +11,21 @@ const getAllHebergement = (req, res) => {
     })
 }
 
-module.exports = {getAllHebergement}
+//Recupere les hebergements par ID
+const getHebergementById = (req, res) => {
+    const id = req.params.id
+    const sql = 'SELECT * FROM hebergements WHERE id = ?'
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            return res.status(500).json({message : "Erreur Serveur"})
+        }
+        if (result.length === 0) {
+            return res.status(404).json({message : "Aucun hebergement trouvé"})
+        }
+        return res.status(200).json(result)
+    })
+}
+
+
+
+module.exports = {getAllHebergement, getHebergementById}
