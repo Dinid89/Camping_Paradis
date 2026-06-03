94057d9fce974cb4054953a742a25739386bb5ba
David A <david.amelin62@gmail.com>
2026-06-03 14:18:52 +0200

    Sécurité des champs a l'enregistrement du compte

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const {register, login} = require('../controllers/authController')
 const { authLimiter } = require('../middlewares/rateLimiter')
+const {validate, validateRegister} = require('../middlewares/validateMiddleware')
 const router = express.Router()
 
 
-router.post('/register', authLimiter, register)
+router.post('/register', authLimiter, validateRegister, validate, register)
 router.post('/login', authLimiter, login)
 
 module.exports = router
